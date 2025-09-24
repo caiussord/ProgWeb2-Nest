1f78115ec4c5b8f1c17708f90fec10f27a886e56
Caius Sordillo <141962436+caiussord@users.noreply.github.com>
2025-09-23 21:06:07 -0300

    AULA_20250923

---
+++ prisma/migrations/20250923224326_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "public"."Courses" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Courses_pkey" PRIMARY KEY ("id")
+);
